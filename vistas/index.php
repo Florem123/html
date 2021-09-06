ec96928b2c193a95a751c4ab3710d4ba4f27370e
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-09-06 00:53:22 -0300

    Update index.php

--- vistas/index.php
+++ vistas/index.php
@@ -6,7 +6,9 @@ session_start();
 if ($_SESSION["s_usuario"] === null){
     header("Location: ../index.php");
 }
-
+if((time() - $_SESSION['s_time']) > 60){
+	header('location: ../bd/logout.php');
+}
 var_dump($_SESSION["s_time"]);
 
 ?>
