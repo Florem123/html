3bed35714130e38f8b549f04068a9f6cd101fa20
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-09-29 15:46:56 -0300

    Update biblio.php

--- vistas/biblio.php
+++ vistas/biblio.php
@@ -50,7 +50,7 @@ if(mysqli_num_rows($sql) == 0){
 			if(mysqli_num_rows($sql) != 0){
 				while($row = mysqli_fetch_assoc($sql)){
 					echo '
-					<p class='titulo'>'.$row['titulo'].'</p>
+					<p class="titulo">'.$row['titulo'].'</p>
 					<pre>'.$row['descripcion'].'</pre>'; 
 					}
 			}?>
