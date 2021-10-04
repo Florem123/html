65f6bc403df712fa616e825ca8607734cb1758a1
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-04 18:41:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if (isset($_GET['sso'])) {
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>ABM-Banco OVA</title>
+    <title>Banco OVA Unaj</title>
     
      <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
