8abd1ccfaa326e60e8be9930611d50b7dfd0e5b6
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-10 12:05:13 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -117,16 +117,16 @@ require 'vistas/mail/SMTP.php';
                                 $mail->send();
                                 
                                 //MAIL AL USUARIO QUE SE REGISTRA
-                                $mail->isSMTP();                                            //Send using SMTP
-                                $mail->Host       = 'mail.unaj.edu.ar';                    //Set the SMTP server to send through
-                                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                                $mail->Username   = 'noresponder@unaj.edu.ar';                     //SMTP username
-                                $mail->Password   = 'qwew1232';                               //SMTP password
-                                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-                                $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+                                $mail2->isSMTP();                                            //Send using SMTP
+                                $mail2->Host       = 'mail.unaj.edu.ar';                    //Set the SMTP server to send through
+                                $mail2->SMTPAuth   = true;                                   //Enable SMTP authentication
+                                $mail2->Username   = 'noresponder@unaj.edu.ar';                     //SMTP username
+                                $mail2->Password   = 'qwew1232';                               //SMTP password
+                                $mail2->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                                $mail2->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
                                 //Recipients
-                                $mail->setFrom('noresponder@unaj.edu.ar', 'OvaUnaj');
+                                $mail2->setFrom('noresponder@unaj.edu.ar', 'OvaUnaj');
                                 $mail2->addAddress($email);     //Add a recipient
 
                                 //Content
