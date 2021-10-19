5eeece32525f019b5b86fd31488c22694cded743
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-19 11:26:47 -0300

    Create whats.php

---
+++ whats.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="#" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Banco OVA Unaj</title>
+    
+     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="estilos.css">
+    
+    <link rel="stylesheet" href="plugins/sweet_alert2/sweetalert2.min.css">
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" role="navigation">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="img/logo-transparente.png" height="40"></a>
+        <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+            &#9776;
+        </button>
+        <div class="collapse navbar-collapse" id="exCollapsingNavbar">
+            <ul class="nav navbar-nav">
+                <li class="nav-item"><a href="whats.php" class="nav-link">¿Qué son los OVA?</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+	<div class="container">
+		  <div class="container">
+        <div class="content" align="center">
+            <h3>¿Qué son los OVA?</h3>
+
+ 			<video class="img-thumbnail" src="img/ova.mp4" autoplay muted loop controls width="720" height="540"></video>
+ 			<br><br>
+           
+	</div>
+		
+	</div><center>
+	<footer>&copy; Banco de Objetos Virtuales de Aprendizaje-UNAJ <?php echo date("Y");?></footer>
+		</center>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
