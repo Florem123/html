c2d41837fe6820bf88a4faeadf66b721267812de
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-19 12:26:13 -0300

    Update biblio.php

--- vistas/biblio.php
+++ vistas/biblio.php
@@ -18,7 +18,7 @@ if(mysqli_num_rows($sql) == 0){
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<link rel="shortcut icon" href="img/iconunaj.ico" />
+	<link rel="shortcut icon" href="../img/iconunaj.ico" />
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
