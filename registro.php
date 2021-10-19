b50e9c8ffb6bb47d3dafdc06a37da5cda9781d0a
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-19 11:26:40 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -17,7 +17,7 @@ require 'vistas/mail/SMTP.php';
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>ABM-Banco OVA</title>
+    <title>Banco OVA Unaj</title>
     
      <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
