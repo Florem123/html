65ff91948834880ead3033d9ec381bad5a582d59
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-19 22:57:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,9 +20,10 @@ if (isset($_GET['sso'])) {
         $_SESSION['s_time'] = time();
         $usuario=$_SESSION["s_usuario"];
         $tipo=$_SESSION["s_tipo"];
-	    
-        $est2=mysqli_query($con, "INSERT INTO logs(usuario,tipo) 
-	VALUES('$usuario','$tipo')") or die(mysqli_error());
+	date_default_timezone_set("America/Argentina/Buenos_Aires");
+	$fecha_actual=date("Y-m-d H:i:s");
+        $est2=mysqli_query($con, "INSERT INTO logs(usuario,tipo,fecha_logueo) 
+	VALUES('$usuario','$tipo','$fecha_actual')") or die(mysqli_error());
         header("Location: vistas/index.php");
          }
 
