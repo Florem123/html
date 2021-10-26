79557708ef0ba6925473a689f25fc0c8a1233d9d
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-26 11:59:29 -0300

    Update profile.php

--- vistas/profile.php
+++ vistas/profile.php
@@ -18,6 +18,7 @@ if((time() - $_SESSION['s_time']) > 7200){
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<title>Banco OVA Unaj</title>
 
 	<!-- Bootstrap -->
