bd55a0d1c0fb638a9d9bc20e0ad4eeabbe6c8fa6
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-26 13:11:22 -0300

    Update logout.php

--- bd/logout.php
+++ bd/logout.php
@@ -3,6 +3,18 @@ session_start();
 $saml_lib_path = '/var/simplesamlphp/lib/_autoload.php';
 require_once $saml_lib_path;
 
+
+$usuario= $_SESSION["s_usuario"];
+$tipo=$_SESSION["s_tipo"];
+date_default_timezone_set("America/Argentina/Buenos_Aires");
+$fecha_actual=date("Y-m-d H:i:s");
+$obs='Salida';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+$consulta2 ="INSERT INTO logs(usuario,tipo,fecha_logueo,obs) VALUES('$usuario','$tipo','$fecha_actual','$obs')";
+$resultado2 = $conexion->prepare($consulta2);
+$resultado2->execute();
+
 if ($_SESSION["s_tipo"]== 'I'){
     	 //check for sso session and invalidate
     	 $as = new \SimpleSAML_Auth_Simple('default-sp');
