b87e912abd3323f25e57b519d35d6b0c8dce2a1d
Florem123 <52898509+Florem123@users.noreply.github.com>
2021-10-26 13:27:35 -0300

    Update logout.php

--- bd/logout.php
+++ bd/logout.php
@@ -5,7 +5,7 @@ require_once $saml_lib_path;
 include_once 'conexion.php';
 $flag=true;
 
-if($flag==true){
+
 $usuario= $_SESSION["s_usuario"];
 $tipo=$_SESSION["s_tipo"];
 date_default_timezone_set("America/Argentina/Buenos_Aires");
@@ -13,11 +13,8 @@ $fecha_actual=date("Y-m-d H:i:s");
 $obs='Salida';
 $objeto = new Conexion();
 $conexion = $objeto->Conectar();
-$consulta2 ="INSERT INTO logs(usuario,tipo,fecha_logueo,obs) VALUES('$usuario','$tipo','$fecha_actual','$obs')";
-$resultado2 = $conexion->prepare($consulta2);
-$resultado2->execute();
-$flag=false;
-}
+
+
 	
 
 if ($_SESSION["s_tipo"]== 'I'){
@@ -31,6 +28,12 @@ if ($_SESSION["s_tipo"]== 'I'){
         unset($_SESSION["s_mail"]);
 	unset($_SESSION["s_time"]);
 	session_destroy();
+	if($flag==true){
+		$consulta2 ="INSERT INTO logs(usuario,tipo,fecha_logueo,obs) VALUES('$usuario','$tipo','$fecha_actual','$obs')";
+		$resultado2 = $conexion->prepare($consulta2);
+		$resultado2->execute();
+		$flag=false;
+		}
 	header("Location: ../index.php");
 } elseif ($_SESSION["s_tipo"] == 'E'){
         unset($_SESSION["s_usuario"]);
